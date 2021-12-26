178614bd1baaa28bd7d3700ca6349f1bea212905
bertylb <bertylaborga@gmail.com>
2021-12-25 21:35:26 -0300

    add showMap and mapLoadError for iframe googleUbication in home-pages

--- src/app/modules/home/pages/home-pages.component.ts
+++ src/app/modules/home/pages/home-pages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
 import { ProductService } from '@shared/services/products-services/product.service';
 import { ServiceService } from '@shared/services/services-service/service.service';
 
@@ -12,13 +12,25 @@ export class HomePagesComponent implements OnInit {
   homeProductImage: Array<any> = [];
   homeServicesImage: Array<any> = [];
 
-  constructor(public productService: ProductService,
-              public servicesService: ServiceService) {
-    this.homeProductImage = productService.productImages;
-    this.homeServicesImage = servicesService.servicesImages;
+  // GOOGLEMAP CONTIENE LA ETIQUETA IFRAME DE LA UBICACION
+  @ViewChild('googleMap') googleMap!: ElementRef;
+  linkAddress: string = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6768.760662637175!2d-60.92736368484473!3d-31.97770377363809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b5bf8a8aeb20f9%3A0x390f5462ae13c2e8!2sCerrajeria%20Dominguez!5e0!3m2!1ses-419!2sar!4v1639193209399!5m2!1ses-419!2sar';
+  showMap: boolean = true;
+
+  constructor(public productService: ProductService, public servicesService: ServiceService) {
+                this.homeProductImage = productService.productImages;
+                this.homeServicesImage = servicesService.servicesImages;
    }
 
   ngOnInit(): void {
+
+    setTimeout(() => {
+      let srcMap = this.googleMap.nativeElement.src;
+      if(srcMap!=this.linkAddress){
+        this.showMap=false;
+      }
+    }, 2000);
+
   }
 
   productsImages() {
