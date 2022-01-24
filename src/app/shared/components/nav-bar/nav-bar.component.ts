04afc245d79233356c7532f514bb9e5be39e2203
bertylb <bertylaborga@gmail.com>
2022-01-23 22:27:58 -0300

    menu button fixes and responsive design

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,8 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,17 +11,48 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class NavBarComponent implements OnInit {
 
-  public mainMenu: Array<any> = [];
-
   @ViewChild('sidenav') sidenav!: MatSidenav;
-  icon: 'menu' | 'close' = 'menu';
+  destroyed = new Subject<void>();
+  private currentScreenSize: string = '';
+  public mainMenu: Array<any> = [];
+  public icon: 'menu' | 'close' = 'menu';
+  public show: boolean = true;
 
-  constructor() { }
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge'],
+  ]);
+
+  constructor(public breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ])
+    .pipe(takeUntil(this.destroyed))
+    .subscribe(result => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+          if(this.currentScreenSize ==='Small' || this.currentScreenSize === 'XSmall'){
+            this.show = false;
+            this.icon = 'menu'
+          }else{
+            this.show = true;
+          }
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.addLinks();
   }
-
   addLinks() {
     this.mainMenu = [
       {
@@ -39,15 +73,12 @@ export class NavBarComponent implements OnInit {
       }
     ]
   }
-  // AL PRESIONAR TECLA ESC
-  close() {
-    this.sidenav.close();
-  }
   changeIcon() {
-    if(this.sidenav.opened) {
-      this.icon = 'close'
-    } else {
-      this.icon = 'menu'
-    }
+    this.sidenav.opened ? this.icon = 'close' : this.icon = 'menu';
+  }
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
   }
 }
+
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -18,7 +19,8 @@ import { PageModelComponent } from './components/page-model/page-model.component
     CommonModule,
     RouterModule,
     MatSidenavModule,
-    MatIconModule
+    MatIconModule,
+    LayoutModule
   ],
   exports: [
     NavBarComponent,
